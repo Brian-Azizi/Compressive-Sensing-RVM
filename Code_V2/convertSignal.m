ad29036ae8a79819c148ae3b54a666d2c2a30311
Brian-Azizi <ba308@cam.ac.uk>
2016-07-31 13:52:29 +0100

    There is a bug when it comes to large files: A) For the 288x352x288 signal, C++ misses out data when saving the reconstructed signal to file. (but not the corrupted - so perhaps an issue with memory. I could try using floats instead of doubles, but it is not completely trivial to make the change). It seems that the first 192 frames were saved fine, but the rest are missing. B) Matlab is unable to convert such large signals to video. Even with the original signal it ran into problems (and the error messages were not usefal at all). There is some error in the way I'm using its VideoWriter functions (implay seems to be able to play back the video). I will test a smaller signal size to see if it works for that. Fixing the bug may take a day and I don't have time at the moment to do it properly, so I'm just gonna hope I can rely on the code to work for signals up to size 256x256x256, say.
    Former-commit-id: 2006f2af28c6cffca13fcf3df0a2975700f65559

--- Code_V2/convertSignal.m
+++ Code_V2/convertSignal.m
@@ -1,83 +1,78 @@
 function convertSignal(FLS_file,frameRate)
-addpath ML_Headers
-
-outputFilenames = FLS_file;
-
-% %outputFilenames = './rvmOutputFilenames.txt';
-% *** options ***
-if nargin < 2
-    frameRate = 30;
-end
-
-% if ~exist('frameRate','var')    
-%     frameRate = 30;
-% end
-
-fid = fopen(outputFilenames);
-settings = textscan(fid,'%s %s');
-fclose(fid);
-N = size(settings{1},1);
-var = matlab.lang.makeValidName(settings{1});
-files = settings{2};
-corridx = 0;
-for i = 1:N    
+  addpath ML_Headers
+  outputFilenames = FLS_file;
+  
+  % *** options ***
+  if nargin < 2
+    frameRate = 25;
+  end
+  
+  % *** open FLS file ***
+  fid = fopen(outputFilenames);
+  settings = textscan(fid,'%s %s');
+  fclose(fid);
+  
+  % *** get variable names and file paths ***
+  N = size(settings{1},1);
+  var = matlab.lang.makeValidName(settings{1});
+  files = settings{2};
+  
+  corridx = 0;				% index of corrupted file
+  
+  % *** open files ***
+  for i = 1:N    
     file = files{i};
     eval([var{i} '= loadSignal(file);']);    
     if strcmp(var{i}, 'corrupted')
-        corridx = i;
+      corridx = i;
     end
-end
-
-eval(['[height, width, frames] = size(' var{1} ');']);
-
-% get output directory
-outputDirTemp = strsplit(files{corridx},'/');
-%outputDir = outputDirTemp{1};
-outputDir = '';
-for k = 1:length(outputDirTemp) - 1
+  end
+  
+  
+  % *** get output directory ***
+  outputDirTemp = strsplit(files{corridx},'/');
+  outputDir = '';
+  for k = 1:length(outputDirTemp) - 1
     outputDir = strcat(outputDir,outputDirTemp{k},'/');
-end
-
-% convert files
-    for i = 1:N
-        % trying out something: name of output files is same as input
-        % files but with appropriate extensions (.png or .avi instead
-        % of .txt)            
-        split = strsplit(files{i},'/');
-        fname = split{end};
-        split2 = strsplit(fname,'.');
-        stem = split2{1};
-        for k = 2:length(split2)-1
-            stem = strcat(stem, '.', split2{k});
-        end
-        
-        %        
-        if frames == 1            
-            name = strcat(outputDir, stem, '.png');
-            if strcmp(var{i}, 'mask')                
-                eval(['imwrite(' var{i} ', name);']);
-            else
-                eval(['imwrite(uint8(' var{i} '), name);']);                                
-            end            
-        else
-            name = strcat(outputDir, stem);
-            %vid = VideoWriter(strcat(mediaOutputDirectory, var{i}), 'Grayscale AVI');
-            vid = VideoWriter(name, 'Grayscale AVI');
-            vid.FrameRate = frameRate;
-            open(vid);
-            for j = 1:frames
-                if strcmp(var{i}, 'mask')
-                    eval(['writeVideo(vid,' var{i} '(:,:,j));']);
-                else
-                    eval(['writeVideo(vid, uint8(' var{i} '(:,:,j)));']);
-                end
-            end
-            close(vid);
-        end
+  end
+  
+  % *** convert files ***
+  for i = 1:N
+    disp(['Converting   ' var{i} ' ...']);
+    % name of output files is same as input files but with 
+    % appropriate extensions (.png or .avi instead of .txt)            
+    split = strsplit(files{i},'/');
+    fname = split{end};
+    split2 = strsplit(fname,'.');
+    stem = split2{1};
+    for k = 2:length(split2)-1
+      stem = strcat(stem, '.', split2{k});
     end
-    disp(['Media output saved in directory:        ' outputDir]);
-
+    
+    % get dimensions
+    eval(['[height, width, frames] = size(' var{i} ');']);
+    
+    if frames == 1			% image
+      name = strcat(outputDir, stem, '.png');
+      if strcmp(var{i}, 'mask')                
+	eval(['imwrite(' var{i} ', name);']);
+      else
+	eval(['imwrite(uint8(' var{i} '), name);']);                                
+      end            
+    else				% video
+      name = strcat(outputDir, stem);
+      vid = VideoWriter(name, 'Grayscale AVI');
+      vid.FrameRate = frameRate;
+      open(vid);
+      for j = 1:frames
+	if strcmp(var{i}, 'mask')
+	  eval(['writeVideo(vid,' var{i} '(:,:,j));']);	  
+	else
+	  eval(['writeVideo(vid, uint8(' var{i} '(:,:,j)));']);	  
+	end
+      end
+      close(vid);
+    end
+  end
+  disp(['Media output saved in directory:        ' outputDir]);  
 end
-% clear fid file h i outputFilenames settings ans ...
-%     displayOutput saveAsMedia var files N j vid ...
-%    mediaOutputDirectory name split fname split2 stem name
